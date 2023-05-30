e81c97acacf35e1e5eba722abc1aeedb73996342
adambushman <adam.bushman1@gmail.com>
2023-05-30 21:21:59 +0000

    New simulation for multiple available players

--- draft/odds_players_are_available.R
+++ draft/odds_players_are_available.R
@@ -13,6 +13,8 @@ for(j in 1:1000) {
 }
 
 tibble(player = results) %>% nrow()
+
+tibble(player = results) %>%
   tibble::rowid_to_column("sim_num") %>%
   unnest_longer(player) %>%
   group_by(player) %>%
@@ -23,3 +25,47 @@ tibble(player = results) %>% nrow()
     perc = freq / overall
   )
   
+
+  
+run_odds_draft <- function(targets) {
+  unavailable = c()
+  len = max(as.integer(names(targets)))
+  
+  for(i in 1:len) {
+    
+    left_over <-
+      max_rank %>%
+      filter(m_pick <= i & !(Player %in% unavailable))
+    
+    if(as.character(i) %in% names(targets)) {
+      if(targets[[as.character(i)]] %in% unavailable) {
+        return(FALSE)
+      }
+    }
+    
+    if(nrow(left_over) > 0) {
+      unavailable[length(unavailable)+1] = left_over$Player[1]
+    }
+    else {
+      unavailable[length(unavailable)+1] = makeSelection(i, unavailable)
+    }
+  }
+  
+  return(TRUE)
+}
+
+players <- list(
+  "9" = "Taylor Hendricks", 
+  "16" = "Jalen Hood-Schifino"#, 
+  #"28" = "Dariq Whitehead"
+)
+
+results = c()
+
+for(i in 1:100) {
+  results[length(results) + 1] = run_odds_draft(players)
+}
+
+length(results[results])/length(results)
+
+
