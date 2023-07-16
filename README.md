a551d3b95f624b7a0cd9cf4077326fbe78fe6359
Adam Bushman <adam.bushman1@gmail.com>
2023-07-15 23:25:24 -0600

    Added README

---
+++ README.md
@@ -0,0 +1,14 @@
+![Bball Data Banner](./bball-data-banner.png)
+
+# Basketball Data
+
+Personal data projects related to my favorite sport. 
+
+## Description
+
+A series of data and R files documenting my small individual projects and the core
+data files that feed many other projects.
+
+## Helpful Resources
+
+[{hoopR} package](https://hoopr.sportsdataverse.org/)
---
+++ summer-league_stat-lines_2023.R
@@ -0,0 +1,45 @@
+library('tidyverse')
+
+data <- read.csv(
+  'C:/Users/Adam Bushman/Documents/R/basketball-data/summer-league_player_2023.csv'
+) %>%
+  as_tibble() %>%
+  janitor::clean_names()
+
+per_30 <- function(min, x) {
+  return(round(x * 30 / min, 1))
+}
+
+totals <- function(gp, x) {
+  return(round(gp * x))
+}
+
+
+new_data <-
+  data %>%
+  select(player, gp, min, fga, fta, pts, oreb, reb, ast, tov, stl, blk, pf) %>%
+  mutate(
+    across(-c(player, gp), ~totals(gp, .))
+  ) %>%
+  group_by(player) %>%
+  summarise(across(everything(), sum)) %>%
+  ungroup() %>%
+  filter(min >= 50) %>%
+  mutate(
+    tsp = pts / ((fga + (fta * 0.44)) * 2), 
+    ast_tov = ast / tov, 
+    act = stl + blk - pf
+  ) %>%
+  select(
+    player, min, pts, reb, ast, tov, stl, blk, pf, tsp, ast_tov, act
+  ) %>%
+  mutate(
+    across(c(pts:act, -tsp), ~per_30(min, .))
+  )
+
+
+new_data %>%
+  mutate(across(pts:act, percent_rank, .names = "{.col}_ptile", .unpack = TRUE)) %>%
+  mutate(across(c(tov_ptile, pf_ptile), ~(1 - .))) %>%
+  filter(player == 'Keyonte George') %>%
+  glimpse()
